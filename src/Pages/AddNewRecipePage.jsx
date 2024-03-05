61ecda9a7251b410a3b6af865005f868e69b3c7e
MonicaAgrela <monicacosta_6@hotmail.com>
2024-03-05 20:40:03 +0000

    fix a problem  in the add new recipe

--- src/Pages/AddNewRecipePage.jsx
+++ src/Pages/AddNewRecipePage.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function AddNewRecipePage() {
@@ -8,11 +8,44 @@ function AddNewRecipePage() {
   const [cuisine, setCuisine] = useState("");
   const [image, setImage] = useState("");
   const [ingredients, setIngredients] = useState("");
+  const [ingredientsTotal, setIngredientsTotal] = useState([]);
   const [instructions, setInstructions] = useState("");
+  const [instructionsTotal, setInstructionsTotal] = useState([]);
   const [description, setDescription] = useState("");
+  const [restaurants, setRestaurants] = useState("");
+  const [restaurantsTotal, setRestaurantsTotal] = useState([]);
 
   const navigate = useNavigate();
-  const { recipesId } = useParams();
+
+  function addIngredient() {
+    setIngredientsTotal([...ingredientsTotal, ingredients]);
+  }
+
+  useEffect(() => {
+    setIngredients(() => {
+      return "";
+    });
+  }, [ingredientsTotal]);
+
+  function addInstruction() {
+    setInstructionsTotal([...instructionsTotal, instructions]);
+  }
+
+  useEffect(() => {
+    setInstructions(() => {
+      return "";
+    });
+  }, [instructionsTotal]);
+
+  function addRestaurant() {
+    setRestaurantsTotal([...restaurantsTotal, restaurants]);
+  }
+
+  useEffect(() => {
+    setRestaurants(() => {
+      return "";
+    });
+  }, [restaurantsTotal]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -22,9 +55,10 @@ function AddNewRecipePage() {
       name: name,
       cuisine: cuisine,
       image: image,
-      ingredients: ingredients,
-      instructions: instructions,
+      ingredients: ingredientsTotal,
+      instructions: instructionsTotal,
       description: description,
+      restaurants: restaurantsTotal,
     };
 
     axios
@@ -113,15 +147,19 @@ function AddNewRecipePage() {
           >
             Ingredients
           </label>
-          <textarea
+          <input
             id="ingredients"
             name="ingredients"
             type="text"
+            value={ingredients}
             onChange={(e) => {
               setIngredients(e.target.value);
             }}
             className="border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:border-indigo-500"
-          ></textarea>
+          ></input>
+          <button type="button" onClick={addIngredient}>
+            Add Ingredient
+          </button>
         </div>
 
         <div className="flex flex-col">
@@ -131,15 +169,19 @@ function AddNewRecipePage() {
           >
             Instructions
           </label>
-          <textarea
+          <input
             id="instructions"
             name="instructions"
             type="text"
+            value={instructions}
             onChange={(e) => {
               setInstructions(e.target.value);
             }}
             className="border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:border-indigo-500"
-          ></textarea>
+          ></input>
+          <button type="button" onClick={addInstruction}>
+            Add Instruction
+          </button>
         </div>
 
         <div className="flex flex-col">
@@ -159,20 +201,29 @@ function AddNewRecipePage() {
             className="border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:border-indigo-500"
           ></textarea>
         </div>
+        <div className="flex flex-col">
+          <label htmlFor="type" className="text-sm font-semibold text-gray-600">
+            Restaurants
+          </label>
+          <input
+            id="restaurants"
+            name="restaurants"
+            type="text"
+            value={restaurants}
+            onChange={(e) => {
+              setRestaurants(e.target.value);
+            }}
+            className="border border-gray-300 rounded px-3 py-2 mt-1 focus:outline-none focus:border-indigo-500"
+          />
+          <button type="button" onClick={addRestaurant}>
+            Add Restaurant
+          </button>
+        </div>
 
-        <button
-          type="submit"
-          className="bg-primary text-white font-semibold py-2 px-4 rounded hover:bg-primaryDark transition duration-300"
-        >
+        <button className="bg-primary text-white font-semibold py-2 px-4 rounded hover:bg-primaryDark transition duration-300">
           Add New Recipe
         </button>
       </form>
-      <Link
-        to={`/recipes/${recipesId}/edit`}
-        className="block text-sm text-gray-600 mt-4"
-      >
-        Edit the Recipe
-      </Link>
     </div>
   );
 }
